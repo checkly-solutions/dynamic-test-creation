73aed0e82f927558ca2b884874eec882f8376433
jonathan <joncanales2@gmail.com>
2024-08-29 10:57:48 -0400

    added service tags to browser check for smoke  tests of a particular bank.

---
+++ __checks__/browser.check.ts
@@ -0,0 +1,22 @@
+//@ts-nocheck
+import { BrowserCheck, RetryStrategyBuilder } from 'checkly/constructs';
+import { defaults } from '../tests/defaults';
+import * as path from 'path';
+
+const serviceTags = defaults.services.map((service) => service.serviceName);
+
+console.log(serviceTags);
+
+new BrowserCheck(`next-danube`, {
+  name: `${defaults.bank} Services Smoke - browser`,
+  frequency: 5,
+  retryStrategy: RetryStrategyBuilder.linearStrategy({
+    baseBackoffSeconds: 5,
+    maxRetries: 1,
+    sameRegion: false,
+  }),
+  tags: serviceTags,
+  code: {
+    entrypoint: path.join(__dirname, '../tests/smoke/smoke-bank.spec.ts'),
+  },
+});
---
+++ tests/smoke/smoke-bank.spec.ts
@@ -0,0 +1,20 @@
+// @ts-nocheck
+import { test, expect } from '@playwright/test';
+import { defaults } from '../defaults';
+
+console.log(defaults.bank);
+
+test.describe(`Bank: ${defaults.bank}`, () => {
+  defaults.services.forEach((service) => {
+    test(`Visit ${service.serviceName}`, async ({ page }) => {
+      // Visit the service URL
+      const response = await page.goto(service.serviceUrl);
+
+      // Check if the status is 200
+      expect(response?.status()).toBe(200);
+
+      // Take a screenshot and compare with baseline
+      await expect(page).toHaveScreenshot(`${service.serviceName}.png`);
+    });
+  });
+});
